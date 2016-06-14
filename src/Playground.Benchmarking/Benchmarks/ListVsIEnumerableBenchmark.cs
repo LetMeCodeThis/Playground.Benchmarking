6bc34812679fbcad09ac69c0e0f3d4be89f8e96d
LetMeCodeThis <LetMeCodeThis@gmail.com>
2016-06-14 22:45:29 +0200

    ListVsIEnumerableBenchmark update

--- src/Playground.Benchmarking/Benchmarks/ListVsIEnumerableBenchmark.cs
+++ src/Playground.Benchmarking/Benchmarks/ListVsIEnumerableBenchmark.cs
@@ -9,37 +9,20 @@
     [Config(typeof(DefaultConfig))]
     public class ListVsIEnumerableBenchmark
     {
-        private List<string> emptyList;
         private List<string> list;
-        private IEnumerable<string> emptyEnumerable;
         private IEnumerable<string> enumerable;
+
+        [Params(0, 10, 100, 1000, 10000)]
+        public int CollectionSize;
 
         [Setup]
         public void Setup()
         {
-            this.emptyList = new List<string>();
-            this.list = new List<string> { "text1", "text2", "text3", "text4", "text5", "text6" };
-            this.emptyEnumerable = new List<string>();
+            this.list = this.GetList(this.CollectionSize);
             this.enumerable = this.list;
         }
 
         [Benchmark(Baseline = true)]
-        public void EmptyListEnumeration()
-        {
-            foreach (var item in this.emptyList)
-            {
-            }
-        }
-
-        [Benchmark]
-        public void EmptyIEnumerableEnumeration()
-        {
-            foreach (var item in this.emptyEnumerable)
-            {
-            }
-        }
-
-        [Benchmark]
         public void ListEnumeration()
         {
             foreach (var item in this.list)
@@ -53,6 +36,18 @@
             foreach (var item in this.enumerable)
             {
             }
+        }
+
+        private List<string> GetList(int collectionSize)
+        {
+            var list = new List<string>(collectionSize);
+
+            for (var i = 0; i < collectionSize; i++)
+            {
+                list.Add("value");
+            }
+
+            return list;
         }
     }
 }
